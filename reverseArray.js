a135b50037a095a7c8bbc25d4f8af0298712fb59
didemertens <d.g.j.mertens@gmail.com>
2020-04-23 09:53:46 +0100

    reverse array with characters

---
+++ reverseArray.js
@@ -0,0 +1,13 @@
+// Write a function that takes an array of characters and reverses the letters in place. 
+
+function reverseArray(array) {
+  // return array.reverse()
+  const swappedArray = []
+
+  array.forEach((char, i) => {
+    swappedArray.push(array[array.length - (i + 1)])
+  })
+  return swappedArray
+}
+
+console.log(reverseArray(['a', 'b', 'c', 'd']))
